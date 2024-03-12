a034d694f0ebf4294b1fca6cb297699f9070b986
Thomas Souchet <thomassouchet22@gmail.com>
2024-03-12 16:20:24 +0100

    Nav login page

--- js/login.js
+++ js/login.js
@@ -5,23 +5,23 @@ const newAccountPage = document.querySelector("#newAccountPage")
 function switchPage(p) {
     switch (p) {
         case "mdpForget":
-            pswdForgetPage.style.display = 'block'
+            pswdForgetPage.style.display = 'flex'
             loginPage.style.display = 'none'
             newAccountPage.style.display = 'none'
             break
         case "newAccount":
             pswdForgetPage.style.display = 'none'
             loginPage.style.display = 'none'
-            newAccountPage.style.display = 'block'
+            newAccountPage.style.display = 'flex'
             break
         case "backNewAccount":
             pswdForgetPage.style.display = 'none'
-            loginPage.style.display = 'block'
+            loginPage.style.display = 'flex'
             newAccountPage.style.display = 'none'
             break
         case "backMdpForget":
             pswdForgetPage.style.display = 'none'
-            loginPage.style.display = 'block'
+            loginPage.style.display = 'flex'
             newAccountPage.style.display = 'none'
             break
     }
@@ -42,3 +42,22 @@ document.querySelector("#backNewAccount").addEventListener('click', (e) => {
 document.querySelector("#backMdpForget").addEventListener('click', (e) => {
     switchPage(e.target.id)
 })
+
+// Vérif email
+const loginForm = document.querySelector('#login form');
+const emailInput = document.querySelector('#login form .em');
+const emailError = document.querySelector('#emailError');
+
+loginForm.addEventListener('submit', function(event) {
+    if (!isValidEmail(emailInput.value)) {
+        emailError.style.display = 'block'; // Afficher le message d'erreur
+        event.preventDefault(); // Empêcher l'envoi du formulaire si l'email n'est pas valide
+    } else {
+        emailError.style.display = 'none'; // Cacher le message d'erreur si l'email est valide
+    }
+});
+
+function isValidEmail(email) {
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
